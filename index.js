2f515dace7aaef03096a35bc78388ce44123e98f
florasantana <39812840+Floraluiza@users.noreply.github.com>
2021-09-23 22:56:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//for - para
+
+/*
+for(var cont = 0; cont < 10; cont++){
+  alert('Contador = ' + cont)
+
+}
+
+for(var cont = 0; cont < 10; cont=cont+2){
+  alert('Contador = ' + cont)
+
+}
+
+for(var cont = 0; cont < 10; cont+=2){
+  alert('Contador = ' + cont)
+
+}
+
+
+*/
+
+//escolha
+ 
+var opcao = parseInt(prompt('Digite a opção '))
+
+switch(opcao){
+    
+    case 1:
+      alert("Voce escolheu opcao 1")
+      break;
+
+    case 2:  
+      alert("Voce escolheu opcao 2")
+      break;
+
+    case 3:  
+      alert("Voce escolheu opcao 3")
+      break;
+
+    default:
+      alert("Opção inválida")
+
+}
